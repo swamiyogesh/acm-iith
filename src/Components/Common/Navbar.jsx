a7333f013fea889b7194b2d8061040762c5720bb
Deeptanshu-sankhwar <deep.shanky@gmail.com>
2020-09-09 11:48:19 +0530

    mobile-design-final-push

--- src/Components/Common/Navbar.jsx
+++ src/Components/Common/Navbar.jsx
@@ -33,13 +33,14 @@ function Navbar()   {
         </BrowserView>
 
         <MobileView>
+
             <nav className="navbar navbar-expand fixed-top">
                 <div className="container">
                     <a className="navbar-brand" href="/acm-iith/#" style={{ textDecoration: 'none', borderBottom: '3px solid #f05a22', top: '50%' }}>ACM | IIT Hyderabad</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarText" style={{marginLeft: "70%"}}>
+                    <div className="collapse navbar-collapse" id="navbarText">
                         <NavDropdown title="Menu" id="basic-nav-dropdown">
                             <NavDropdown.Item href="/acm-iith/#">Home</NavDropdown.Item>
                             <NavDropdown.Item href="/acm-iith/#/about">About Us</NavDropdown.Item>
