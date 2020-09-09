b7199ee17fa88efc22691ade44bde4f1f52bd00a
Deeptanshu-sankhwar <deep.shanky@gmail.com>
2020-09-10 02:43:24 +0530

    design-fix

--- src/Components/Common/Navbar.jsx
+++ src/Components/Common/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar()   {
                      <span className="navbar-toggler-icon"></span>
                      </button>
                      <div className="collapse navbar-collapse" id="navbarText">
-                     <span className="navbar-text right-links" style={{marginLeft: "-27%"}}>
+                     <span className="navbar-text right-links" style={{marginLeft: "-28%"}}>
                          {/* <a href="/events">Events</a> */}
                          {/* <a href="/blogs">Blog</a> */}
                          {/* <a href="/team">Team</a> */}
