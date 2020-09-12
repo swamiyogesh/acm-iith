4dc8901da10ac26f36ef98e9f2d0e3ff92b02e2f
Deeptanshu-sankhwar <deep.shanky@gmail.com>
2020-09-13 01:23:02 +0530

    late-night-fix

--- src/About.js
+++ src/About.js
@@ -13,14 +13,13 @@ function About()   {
                 <br /><br /><br />
                 <p className="h35" style={{color: 'black', textDecoration: 'none', borderLeft: '5px solid #479ff8', top: '50%'}}>&nbsp;About Us</p>
                 <br />
-                <a className="h22" style={{color: 'black', textDecoration: 'none',  top: '50%'}}>About Us</a>
-                <br />
-                <p className="p20">IIT Hyderabad ACM student chapter has been officially chartered on 31st August, 2020 under the shelter of department of Computer Science and Engineering. The chapter will organize events which includes talks by renowned speakers, workshops, etc, allowing the students to gain an exposure in the computing world as well as to understand the advancements happening in computer science worldwide.
-                <br /><br   />
-                <a className="h22" style={{color: 'black', textDecoration: 'none',  top: '50%'}}>About Us</a>
-                <br />
-                
-                The chapter aims at bringing together computer fraternity and cultivate the research and development oriented skills among students. It attempts to create oppurtunities for networking, mentoring and bonding with peers and experts across the computing spectrum.  
+                <p className="p20">                
+                IIT Hyderabad ACM student chapter was officially chartered on 31st August, 2020 under the shelter of Computer Science and Engineering department at IIT Hyderabad. This student driven chapter is divided into four Interest groups namely, <a href="/acm-iith/#/info/machine-intelligence">Machine Intelligence</a>, <a href="/acm-iith/#/info/systems-and-network">Systems and Networks</a>, <a href="/acm-iith/#/info/theoretical-cs">Theoretical Computer Science</a> and <a href="/acm-iith/#/info/women-in-cs">Women in Computer Science</a>. The chapter will organize events which include talks by renowned speakers, organise reading groups and help in fostering research collaborations among the undergraduate and graduate students working in various disciplines of  Computer Science. 
+The primary goal of this chapter is to help students gain exposure to cutting edge research in various disciplines of Computer Science and help them take part in research activities that focus on advancing their academic and research careers. 
+If you’re working on exciting research projects and would like to present your research, feel free to drop us a mail at <a href="mailto: acm.chapter@iith.ac.in">acm.chapter@iith.ac.in.</a> 
+                </p>
+                <p className="p20">
+                The chapter aims at bringing together computer fraternity and cultivate the research and development oriented skills among students. It attempts to create oppurtunities for networking, mentoring and bonding with peers and experts across the computing spectrum.
                 </p>
 
                 <a href="/acm-iith/#/joinus" style={{fontSize: '24px', color: "#080e2c"}}><b>Computing enthusiast enough? Become a member!</b></a>
--- src/Groups/WomenInCS.js
+++ src/Groups/WomenInCS.js
@@ -15,7 +15,11 @@ function WomenInCS()  {
                 </BrowserView>
                 <MobileView>
                     <p className="h35" style={{color: 'black', textDecoration: 'none', borderLeft: '5px solid #479ff8', top: '50%'}}>&nbsp;Women in &nbsp;Computer &nbsp;Science</p>
-                </MobileView>            
+                </MobileView>
+                <br /><br />
+                <p className="p20">
+                Women in Computer Science group is a part of ACM IIT Hyderabad Student Chapter which works towards promoting and supporting the growing community of women in Computer Science and Technology alongside raising awareness about the issues which women face in the field and helping them by encouraging their educational and professional development. We aim at creating academic, social and professional opportunities for women in  Computer Science by promoting the breadth of the field and its diverse community, thereby striving for a healthy and supportive community atmosphere for all. Making a difference and solving problems serves as our basic motivating purpose.
+                </p>       
             </div>
         </div>
     )
