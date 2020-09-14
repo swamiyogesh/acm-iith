8d3d697d9db2dc2b911aa868887c2768626ab10f
Deeptanshu-sankhwar <deep.shanky@gmail.com>
2020-09-15 02:59:05 +0530

    google-sheets-api-added

--- src/Joinus.js
+++ src/Joinus.js
@@ -1,101 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './css/style.css';
 import './css/common.css';
 import './css/join.css';
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Joinus()  {
-    // backgroundImage: "-moz-linear-gradient( 136deg, #479ff8, #fff)"
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		number: ''
+	})
+
+	const { name, email, number } = formData
+
+	const handleChange = text => e => {
+		setFormData({ ...formData, [text]: e.target.value });
+    }
+	const appendSpreadsheet = async(row) =>	{
+		toast.success('Thanks for your response. Our team will get back to you shortly.')
+		const doc = new GoogleSpreadsheet('1iRewg0yJTdNm27chtnI_g5r245J11dLdSqT5Qip0yBI');
+
+		await doc.useServiceAccountAuth(require('./peak-radius-289518-892f6a16e540.json'));
+		await doc.loadInfo(); // loads document properties and worksheets
+		const sheet = doc.sheetsByIndex[0];
+		await sheet.addRow(row);
+	}
+	
     return (
-        <div className="Joinus" style={{backgroundColor:  "#F0F8FF", height: "100vh"}}>
-                
-            
-            <div class="page-content">
-		    <div class="form-v1-content">
-			<div class="wizard-form">
-		        <form class="form-register" action="#" method="post">
-		        	<div id="form-total">
-		        		
-			            <h2>
-			            	<p class="step-icon" style={{marginTop: "6%"}}><span>01</span></p>
-			            	<span class="step-text">Value proposition</span>
-			            </h2>
-			            <section>
-			                <div class="inner">
-                                            <div class="wizard-header" style={{padding: "7%"}}>
-                                            <h5>Become a IIT Hyderabad ACM Student Chapter member!</h5></div>
-			                	<div class="form-holder">
+        <div className="Joinus" style={{backgroundColor: "#F0F8FF", height: "100vh"}}>
+			
+			<div className="container">
+			<ToastContainer />
+				<br /><br />
+				<div className="container">
+				<div className="container">
+				<div className="container">
+				<div className="primaryContainer">
+					<div style={{width: '50%', backgroundColor: '#2B547E', float: 'left'}}>
+						<br  />
+						<center><p className="h30" style={{color: "#fff"}}>Become a IIT Hyderabad ACM Student Chapter member!</p></center>
+						<br />
+						<div className="container" style={{color: "#fff", paddingLeft: '10%', paddingRight: '5%'}}>
+						<p className="p18">- Chapter members are eligible for an “acm.org” email forwarding address with filtering.</p>
+						<p className="p18">- Members get a full-year electronic subscription to XRDS, ACM's Student Magazine</p>
+						<p className="p18">- Chapter Members are eligible for a three-month complimentary electronic subscription to ACM’s flagship publication Communications of the ACM.</p>
+						<p className="p18">- Members receive ACM's Popular E-Newsletters, TechNews, CareerNews and MemberNet</p>
+						<p className="p18">- Members get access to ACM Student Quick Takes (SQT), a quarterly email newsletter with each issue highlighting ACM activities, programs, and offerings of interest.</p>
+						<p className="p18">- Members get to participate in logistics of events.</p><br />
+						</div>
+					</div>
+					<div style={{width: "50%", backgroundColor: '#fff', float: "right"}}>
+						<br /><br />
+						<form className="container" style={{paddingLeft: "5%"}} onSubmit={() => appendSpreadsheet(formData)}>
+							<p className="h22">Name</p>
+							<input className = "primaryInput" type="text" value={name} onChange={handleChange('name')} placeholder="Add Name " /><br /><br />
+
+							<p className="h22">Email</p>
+							<input className = "primaryInput" type="email" value={email} onChange={handleChange('email')} placeholder="Add Email " /><br /><br />
+
+							<p className="h22">Phone Number</p>
+							<input className = "primaryInput" type="number" value={number} onChange={handleChange('number')} placeholder="Add Number " /><br /><br />
+
+							<br /><br /><br /><br /><br />
+							<center><button type='submit' className="primaryButton">Join us</button></center>
+							<br /><br />
+						</form>
+					</div>
 
 
-									<p>-One stop shop for laundry </p>
-                                                                        <p>-Allow Laundromats to stand out</p>
-                                                                        <p>-Bridging the gaps between customer and laundry shops</p>
-                                                                        <p>-Provide rating and review to evaluate laundry shops</p>
-                                                                        <p>-Online ordering and laundry service</p>
-								         <p>-Provide search service</p>
-                                                                         <p>-Managing logistics to processorders</p><br /><br />
-								</div>
-							</div>
-			            </section>
-                                    
-			            <h2>
-			            	<p class="step-icon" style={{marginTop: "6%"}}><span>02</span></p>
-			            	<span class="step-text">Personal Information</span>
-			            </h2>
-			            <section>
-			                <div class="inner">
-			                	<div class="wizard-header" style={{padding: "7%"}}>
-
-									<h5>Please enter your infomation and proceed .  </h5>
-								</div>
-								<div class="form-row">
-									<div class="form-holder">
-										<fieldset>
-											<legend>First Name</legend>
-											<input type="text" class="form-control" id="first-name" name="first-name" placeholder="First Name" required  />
-										</fieldset>
-									</div>
-									<div class="form-holder">
-										<fieldset>
-											<legend>Last Name</legend>
-											<input type="text" class="form-control" id="last-name" name="last-name" placeholder="Last Name" required  />
-										</fieldset>
-									</div>
-								</div>
-								<div class="form-row">
-									<div class="form-holder form-holder-2">
-										<fieldset>
-											<legend>Your Email</legend>
-											<input type="text" name="your_email" id="your_email" class="form-control" pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" placeholder="example@email.com" required />
-										</fieldset>
-									</div>
-								</div>
-								<div class="form-row">
-									<div class="form-holder form-holder-2">
-										<fieldset>
-											<legend>Phone Number</legend>
-											<input type="text" class="form-control" id="phone" name="phone" placeholder="+91 9xxxxxxxxx" required />
-										</fieldset>
-									</div>
-								</div>
-								<div class="form-row form-row-date">
-									<div class="form-holder form-holder-2">
-										<legend>Shop / Company / Individual name:<br></br></legend>
-                                                                      <fieldset>
-                                                                     <input type="text" class="form-control" id="details" name="details" placeholder="Write your option here" required />
-									</fieldset>
-
-									</div>
-								</div>
-                                <br />
-							</div>
-			            </section>
-
-		        	</div>
-		        </form>
+				</div>
+				</div>
+				</div>
+				</div>
 			</div>
-        </div>
-	</div>
-
 
         </div>
     )
