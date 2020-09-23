1f2eb91b8aab1d19142008dd3b19d8b819674e86
Deeptanshu-sankhwar <deep.shanky@gmail.com>
2020-09-23 16:28:25 +0530

    tteam-aded

--- src/Team.js
+++ src/Team.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import './css/style.css';
 import './css/common.css';
-import Navbar from './Components/Common/Navbar'
-import Footer from './Components/Common/Footer'
 
 function Team()   {
     return (
@@ -13,56 +11,100 @@ function Team()   {
             <br /><br />
             <div className="row">
                 <div className="col-md">
-                    {/* <img src={require('./assets/images/iith.png')} style={{borderRadius: "50%", width: "20%", height: "20%"}} /> */}
-
-
-                </div>
-                <div className="col-md">
-                    <div className="primaryContainer">
+                    {/* <div className="primaryContainer"> */}
                         <center>
-                            <p className="h22">Name</p>
-                            <p className="p15">Lorem Epsum!</p>
+                        <img src={require("./assets/images/vineeth.jpg")} style={{borderRadius:"100%",width: "50%",  padding: "2%"}} />
+                            <p className="h22">Vineeth N Balasubramanian</p>
+                            <p className="p15">Faculty Sponsor</p>
                             <br /><br /><br /><br />
                         </center>
-                    </div>
+                    {/* </div> */}
                 </div>
                 <div className="col-md">
-                    <div className="primaryContainer">
+                    {/* <div className="primaryContainer"> */}
                         <center>
-                            <p className="h22">Name</p>
-                            <p className="p15">Lorem Epsum!</p>
-                            <br /><br /><br /><br />
+                            <img src={require("./assets/images/surgan.jpg")} style={{borderRadius:"100%",width: "50%",  padding: "2%"}} />
+                            <br />
+                            <p className="h22">Surgan Jandial</p>
+                            <p className="p15">Chairman</p>
+                            <br /><br />
                         </center>
-                    </div>
+                    {/* </div> */}
                 </div>
-            </div><br />
+                <div className="col-md">
+                    {/* <div className="primaryContainer"> */}
+                        <center>
+                            <img src={require("./assets/images/chaitanya.jpg")} style={{borderRadius:"100%",width: "50%",  padding: "2%"}} />
+                            <br />
+                            <p className="h22">Chaitanya Devaguptapu</p>
+                            <p className="p15">Vice Chairman</p>
+                            <br /><br />
+                        </center>
+                    {/* </div> */}
+                </div>
+            </div>
             <div className="row">
                 <div className="col-md">
-                    <div className="primaryContainer">
+                    {/* <div className="primaryContainer"> */}
                         <center>
-                            <p className="h22">Name</p>
-                            <p className="p15">Lorem Epsum!</p>
-                            <br /><br /><br /><br />
+                            <img src={require("./assets/images/puneet.jpg")} style={{borderRadius:"100%", width: "50%",  padding: "2%"}} />
+                            <br />
+                            <p className="h22">Puneet Mangla</p>
+                            <p className="p15">Machine Intelligence Head</p>
+                            <br /><br />
                         </center>
-                    </div>
+                    {/* </div> */}
                 </div>
                 <div className="col-md">
-                    <div className="primaryContainer">
+                    {/* <div className="primaryContainer"> */}
                         <center>
-                            <p className="h22">Name</p>
-                            <p className="p15">Lorem Epsum!</p>
-                            <br /><br /><br /><br />
+                            <img src={require("./assets/images/vedant.jpg")} style={{borderRadius:"100%", width: "50%",  padding: "2%"}} />
+                            <br />
+                            <p className="h22">Vedant Singh</p>
+                            <p className="p15">Treasurer</p>
+                            <br /><br />
                         </center>
-                    </div>
+                    {/* </div> */}
                 </div>
                 <div className="col-md">
-                    <div className="primaryContainer">
+                    {/* <div className="primaryContainer"> */}
                         <center>
-                            <p className="h22">Name</p>
-                            <p className="p15">Lorem Epsum!</p>
-                            <br /><br /><br /><br />
+                            <img src={require("./assets/images/archana.jpg")} style={{borderRadius:"100%", width: "50%",  padding: "2%"}} />
+                            <br />
+                            <p className="h22">CS Archana</p>
+                            <p className="p15">Design Lead</p>
+                            <br /><br />
                         </center>
-                    </div>
+                    {/* </div> */}
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md">
+                    <center>
+                        {/* <img src={require("./assets/images/vedant.jpg")} style={{borderRadius:"100%", width: "50%",  padding: "2%"}} />
+                        <br />
+                        <p className="h22">CS Archana</p>
+                        <p className="p15">Design Lead</p>
+                        <br /><br /> */}
+                    </center>
+                </div>
+                <div className="col-md">
+                    <center>
+                        <img src={require("./assets/images/deeptanshu.jpg")} style={{borderRadius:"100%", width: "50%",  padding: "2%"}} />
+                        <br />
+                        <p className="h22">Deeptanshu Sankhwar</p>
+                        <p className="p15">Outreach Lead</p>
+                        <br /><br />
+                    </center>
+                </div>
+                <div className="col-md">
+                    <center>
+                        {/* <img src={require("./assets/images/vedant.jpg")} style={{borderRadius:"100%", width: "50%",  padding: "2%"}} />
+                        <br />
+                        <p className="h22">CS Archana</p>
+                        <p className="p15">Design Lead</p> */}
+                        <br /><br />
+                    </center>
                 </div>
             </div>
             <br /><br /><br /><br /><br />
